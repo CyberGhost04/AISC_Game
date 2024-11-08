76b730d82a1c2cc13fb8a69788f8cd5751e92ec7
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-11-08 23:52:57 +0530

    jumping mechanism

--- main.py
+++ main.py
@@ -38,17 +38,25 @@ class Bird:
         self.height = self.y
 
     def move(self):
-        """
-        make the bird move
-        :return: None
-        """
+
         self.tick_count += 1
 
         # for downward acceleration
         displacement = self.vel*(self.tick_count) + 0.5*(3)*(self.tick_count)**2  # calculate displacement
 
-        # terminal velocity
         if displacement >= 16:
             displacement = (displacement/abs(displacement)) * 16
+
+        if displacement < 0:
+            displacement -= 2
+
+        self.y = self.y + displacement
+
+        if displacement < 0 or self.y < self.height + 50:  
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:  
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
 
     
