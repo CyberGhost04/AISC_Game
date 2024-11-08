65c449eda9656086a2dfbd1f5c1f9c5a1fc60514
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-11-08 23:54:48 +0530

    problems in draw function

--- main.py
+++ main.py
@@ -60,11 +60,7 @@ class Bird:
                 self.tilt -= self.ROT_VEL
 
     def draw(self, win):
-        """
-        draw the bird
-        :param win: pygame window or surface
-        :return: None
-        """
+
         self.img_count += 1
 
         # For animation of bird, loop through three images
@@ -79,3 +75,11 @@ class Bird:
         elif self.img_count == self.ANIMATION_TIME*4 + 1:
             self.img = self.IMGS[0]
             self.img_count = 0
+
+        # so when bird is nose diving it isn't flapping
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
+
+
+        #blitRotateCenter(win, self.img, (self.x, self.y), self.tilt)
