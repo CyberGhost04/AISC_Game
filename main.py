74b3d200d032e99ec8f5ca91591d8f866dc8ca2d
PranavTyagi03 <pranavtyagi03@gmail.com>
2024-11-15 23:23:00 +0600

    link config file and setup neat

--- main.py
+++ main.py
@@ -248,3 +248,26 @@ def main():
     quit()
 
 main()
+
+
+def run(config_file):
+    
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    
+    winner = p.run(eval_genomes, 50)
+
+    print('\nBest genome:\n{!s}'.format(winner))
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    run(config_path)
