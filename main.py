6f3c0d6db89df7965823a3b57fa1ec478e6847e3
PranavTyagi03 <pranavtyagi03@gmail.com>
2024-11-16 00:14:14 +0600

    change draw_window def

--- main.py
+++ main.py
@@ -184,19 +184,41 @@ class Base:
         win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(win,bird, pipes, base, score): 
+def draw_window(win, birds, pipes, base, score, gen, pipe_ind):
+    
+    if gen == 0:
+        gen = 1
     win.blit(bg_img, (0,0))
 
-    for pipe in pipes: 
+    for pipe in pipes:
         pipe.draw(win)
 
-    text = STAT_FONT.render("Score: " + str(score), 1, (255,255,255))
-    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
-
     base.draw(win)
+    for bird in birds:
+        
+        if DRAW_LINES:
+            try:
+                pygame.draw.line(win, (255,0,0), (bird.x+bird.img.get_width()/2, bird.y + bird.img.get_height()/2), (pipes[pipe_ind].x + pipes[pipe_ind].PIPE_TOP.get_width()/2, pipes[pipe_ind].height), 5)
+                pygame.draw.line(win, (255,0,0), (bird.x+bird.img.get_width()/2, bird.y + bird.img.get_height()/2), (pipes[pipe_ind].x + pipes[pipe_ind].PIPE_BOTTOM.get_width()/2, pipes[pipe_ind].bottom), 5)
+            except:
+                pass
+        
+        bird.draw(win)
 
-    bird.draw(win)
+    
+    score_label = STAT_FONT.render("Score: " + str(score),1,(255,255,255))
+    win.blit(score_label, (WIN_WIDTH - score_label.get_width() - 15, 10))
+
+    
+    score_label = STAT_FONT.render("Gens: " + str(gen-1),1,(255,255,255))
+    win.blit(score_label, (10, 10))
+
+    
+    score_label = STAT_FONT.render("Alive: " + str(len(birds)),1,(255,255,255))
+    win.blit(score_label, (10, 50))
+
     pygame.display.update()
+
 
 def eval_genomes(genomes, config):
     global WIN, gen
